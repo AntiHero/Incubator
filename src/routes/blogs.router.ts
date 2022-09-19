c64b6026fe5fa029661c7f327fd970239b960feb
Aleksandr Schemelev <prophreaker@gmail.com>
2022-09-20 01:31:41 +0300

    feat: add blogs routes

---
+++ src/routes/blogs.router.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const blogsRouter = Router();
+
+blogsRouter.get('/', /*TODO*/ );
+
+blogsRouter.post('/', /*TODO*/ );
+
+blogsRouter.get('/:id', /*TODO*/ );
+
+blogsRouter.put('/:id', /*TODO*/ );
+
+blogsRouter.delete('/:id', /*TODO*/ );
+
+export default blogsRouter;
