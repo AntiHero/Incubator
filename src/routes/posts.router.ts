c11b1f07b34c2539d0b61441aea1cf53a66032cf
Aleksandr Schemelev <prophreaker@gmail.com>
2022-09-20 01:33:43 +0300

    feat: add posts routes

---
+++ src/routes/posts.router.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+const postsRouters = Router();
+
+postsRouters.get('/' /* TODO */);
+
+postsRouters.post('/' /* TODO */);
+
+postsRouters.get('/:id' /* TODO */);
+
+postsRouters.put('/:id' /* TODO */);
+
+postsRouters.delete('/:id' /* TODO */);
+
+export default postsRouters;
