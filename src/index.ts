aa57ceace363915ca1b3d8e83aa3e2da709bde0c
Aleksandr Schemelev <prophreaker@gmail.com>
2022-09-20 01:26:33 +0300

    chore: add controllers, routes, models folders

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-import express from 'express';
-
-const app = express();
-
-app.set('port', process.env.PORT || 9000);
-
-
+import app from './app';
 
 app.listen(app.get('port'), () => {
   console.log('Server is running at http://localhost:%s', app.get('port'));
