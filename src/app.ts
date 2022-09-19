a1d8514b59018bb616115c5c2dc499d7b697e23d
Aleksandr Schemelev <prophreaker@gmail.com>
2022-09-20 01:37:56 +0300

    feat: add routers to app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 
+import blogsRouter from './routes/blogs.router';
+import postsRouter from './routes/posts.router';
+import testingRouter from './routes/testing.router';
+
 const app = express();
 
 app.set('port', process.env.PORT || 9000);
+
+app.use('/posts', postsRouter);
+app.use('/blogs', blogsRouter);
+app.use('/testing', testingRouter);
 
 export default app;
