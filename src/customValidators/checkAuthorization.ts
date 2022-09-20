3b079bb2366616ddd51a3b259648b6ec484b0239
Aleksandr Schemelev <prophreaker@gmail.com>
2022-09-20 17:00:44 +0300

    feat: add custom auth check validator, provide mapping for validation result into specified shape

---
+++ src/customValidators/checkAuthorization.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Response, Request } from 'express';
+import { header, validationResult } from 'express-validator';
+
+import { Headers } from '../@types';
+import { customValidationResult } from './customValidationResults';
+
+const AUTHENTICATION_SCHEME = 'Basic';
+
+const login = 'admin';
+const password = 'qwerty';
+
+export const checkAuthorization = [
+  header(Headers.Authorization).custom((value: string) => {
+    const [scheme, encodedLoginPassword] = value.split(/\s+/);
+
+    if (scheme !== AUTHENTICATION_SCHEME) {
+      throw new Error('Invalid authentication scheme');
+    }
+
+    const decodedLoginPassowrd = Buffer.from(
+      encodedLoginPassword,
+      'base64'
+    ).toString();
+
+    if (decodedLoginPassowrd !== [login, password].join(':')) {
+      throw new Error('Incorrect login/password pair');
+    }
+    
+    return true;
+  }),
+  (req: Request, res: Response, next: NextFunction) => {
+    if (!customValidationResult(req).isEmpty()) {
+      res.status(401).end();
+
+      return;
+    }
+
+    next();
+  },
+];
---
+++ src/customValidators/customValidationResults.ts
@@ -0,0 +1,12 @@
+import { validationResult } from 'express-validator';
+
+import { FieldError } from '../@types';
+
+export const customValidationResult = validationResult.withDefaults({
+  formatter: (error): FieldError => {
+    return {
+      message: error.msg,
+      field: error.param,
+    };
+  },
+});
