cc67758103bdda52d0c2efa4ed1d89ba85cdd5c3
Aleksandr Schemelev <prophreaker@gmail.com>
2022-09-20 20:59:29 +0300

    feat: add delete blog controller

---
+++ src/controllers/blog/deleteBlog.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+
+import * as blogsRepository from '@/repository/blogs.repository';
+import { checkAuthorization } from '@/customValidators/checkAuthorization';
+
+export const deleteBlog = [
+  ...checkAuthorization,
+  async (req: Request, res: Response) => {
+    const result = blogsRepository.findBlogByIndAndDelete(req.params.id);
+
+    if (result === null) {
+      res.status(404).end();
+
+      return;
+    }
+
+    res.status(204).end();
+  },
+];
