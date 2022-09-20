49692dd65fd1d8c727dc75d3c96f0e4f403e65a0
Aleksandr Schemelev <prophreaker@gmail.com>
2022-09-20 23:29:55 +0300

    chore: add testing router

--- src/routes/testing.router.ts
+++ src/routes/testing.router.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
+import { deleteAll } from "src/controllers/testing/deleteAll";
+
 const testingRouter = Router();
 
-testingRouter.delete('/all-data', /* TODO */);
+testingRouter.delete('/all-data', deleteAll);
 
 export default testingRouter;
