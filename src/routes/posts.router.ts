be5ce003fd469fc2dfd7f6bc5224b1c93f253c78
Aleksandr Schemelev <prophreaker@gmail.com>
2022-09-20 23:35:34 +0300

    feat: add posts router

--- src/routes/posts.router.ts
+++ src/routes/posts.router.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express';
 
+import { getPost } from '../controllers/post/getPost';
+import { postPost } from '../controllers/post/postPost';
+import { deletePost } from '../controllers/post/deletPost';
+import { updatePost } from '../controllers/post/updatePost';
+import { getAllPosts } from '../controllers/post/getAllPosts';
+
 const postsRouters = Router();
 
-postsRouters.get('/' /* TODO */);
+postsRouters.get('/', getAllPosts);
 
-postsRouters.post('/' /* TODO */);
+postsRouters.post('/', postPost);
 
-postsRouters.get('/:id' /* TODO */);
+postsRouters.get('/:id', getPost);
 
-postsRouters.put('/:id' /* TODO */);
+postsRouters.put('/:id', updatePost);
 
-postsRouters.delete('/:id' /* TODO */);
+postsRouters.delete('/:id', deletePost);
 
 export default postsRouters;
