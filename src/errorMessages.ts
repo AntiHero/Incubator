bdeaaa7cff28d50330867b23ff88ffaf40fbcd36
Aleksandr Schemelev <prophreaker@gmail.com>
2022-09-20 17:01:35 +0300

    chore: add custom masages

---
+++ src/errorMessages.ts
@@ -0,0 +1,4 @@
+export const NOT_STRING_ERROR = 'Field should be of type string';
+export const NOT_MATCHNG_PATTER_ERROR = "Field doesn't match provided pattern";
+export const MAX_LENGHTH_ERROR = (max: number) =>
+  `Value should be ${max} characters long`;
