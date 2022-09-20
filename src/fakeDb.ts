7b1f19252bfb3726862bdde277bce8e789640047
Aleksandr Schemelev <prophreaker@gmail.com>
2022-09-20 13:14:17 +0300

    chore: add fake data

---
+++ src/fakeDb.ts
@@ -0,0 +1,19 @@
+export default {
+  blogs: [
+    {
+      id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+      name: 'About everything',
+      youtubeUrl: 'https://www.youtube.com/watch?v=vxM3F7qw050',
+    },
+  ],
+  posts: [
+    {
+      id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6e',
+      title: 'What is next?',
+      shortDescription: 'fake description',
+      content: 'bla-bla-bla',
+      blogId: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+      blogName: 'About everything',
+    },
+  ],
+};
