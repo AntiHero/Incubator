6e6c544a1d11627a8e099ade6a4bd4b78b4adb54
Aleksandr Schemelev <prophreaker@gmail.com>
2022-09-20 13:16:40 +0300

    chore: simplify Blog model

--- src/models/Blog.ts
+++ src/models/Blog.ts
@@ -1,11 +1,27 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import { h02 } from '../@types';
 
 class Blog implements h02.db.BlogViewModel {
   public id: string | null = null;
 
-  public name: string | null = null;
+  constructor (public name: string, public youtubeUrl: string) {
+    this.id = uuidv4();
+    this.name = name;
+    this.youtubeUrl = youtubeUrl;
+  }
 
-  public youtubeUrl: string | null = null;
+  public async save () {
+    return Promise.resolve().then(() => {
+      const blog = {
+        id: this.id,
+        name: this.name,
+        youtubeUrl: this.youtubeUrl,
+      };
+
+      return blog;
+    });
+  }
 }
 
 export default Blog;
