15c5c28c46380fc56f0f0e09720a0a0eb02cb0d4
Aleksandr Schemelev <prophreaker@gmail.com>
2022-10-16 12:50:25 +0300

    feat: add POST /auth/registration

---
+++ src/controllers/auth/registration.ts
@@ -0,0 +1,67 @@
+import { body } from 'express-validator';
+import { Request, Response } from 'express';
+
+import User from '@/models/User';
+import { UserFields } from '@/enums';
+import { APIErrorResult } from '@/@types';
+import * as ErrorMessages from '@/errorMessages';
+import * as usersRepository from '@/repository/users.repository';
+import { checkAuthorization } from '@/customValidators/basicAuthValidator';
+import { validateUserUnicity } from '@/customValidators/uniqueUserValidator';
+import { customValidationResult } from '@/customValidators/customValidationResults';
+
+const MIN_PASSWORD_LEN = 6;
+const MAX_PASSWORD_LEN = 20;
+
+const MIN_LOGIN_LEN = 3;
+const MAX_LOGIN_LEN = 10;
+
+export const registration = [
+  ...checkAuthorization,
+  body(UserFields.login)
+    .isString()
+    .withMessage(ErrorMessages.NOT_STRING_ERROR)
+    .trim()
+    .isLength({ max: MAX_LOGIN_LEN })
+    .withMessage(ErrorMessages.MAX_LENGTH_ERROR(MAX_LOGIN_LEN))
+    .isLength({ min: MIN_LOGIN_LEN })
+    .withMessage(ErrorMessages.MIN_LENGTH_ERROR(MIN_LOGIN_LEN)),
+  body(UserFields.password)
+    .isString()
+    .withMessage(ErrorMessages.NOT_STRING_ERROR)
+    .trim()
+    .isLength({ max: MAX_PASSWORD_LEN })
+    .withMessage(ErrorMessages.MAX_LENGTH_ERROR(MAX_PASSWORD_LEN))
+    .isLength({ min: MIN_PASSWORD_LEN })
+    .withMessage(ErrorMessages.MIN_LENGTH_ERROR(MIN_PASSWORD_LEN)),
+  body(UserFields.email)
+    .isString()
+    .withMessage(ErrorMessages.NOT_STRING_ERROR)
+    .trim()
+    .matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)
+    .withMessage(ErrorMessages.WRONG_PATTERN_ERROR),
+  validateUserUnicity,
+  async (req: Request, res: Response) => {
+    if (!customValidationResult(req).isEmpty()) {
+      res
+        .type('text/plain')
+        .status(400)
+        .send(
+          JSON.stringify({
+            errorsMessages: customValidationResult(req).array({
+              onlyFirstError: true,
+            }),
+          } as APIErrorResult)
+        );
+
+      return;
+    }
+
+    const { login, email, password } = req.body;
+
+    const userData = new User(login, email, password);
+    await usersRepository.createUser(userData);
+
+    res.sendStatus(204);
+  },
+];
--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -2,10 +2,13 @@ import { Router } from 'express';
 
 import { me } from '@/controllers/auth/me';
 import { login } from '@/controllers/auth/login';
+import { registration } from '@/controllers/auth/registration';
 
 const authRouer = Router();
 
 authRouer.post('/login', login);
+
+authRouer.post('/registration', registration);
 
 authRouer.get('/me', me);
 
