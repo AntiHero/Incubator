39b0877760d047f43ff0a3b1d170e9fbee9aa893
Aleksandr Schemelev <prophreaker@gmail.com>
2022-10-18 23:19:27 +0300

    feat: add refresh token invalidation with new JWT SECRET

--- src/controllers/auth/logout.ts
+++ src/controllers/auth/logout.ts
@@ -2,12 +2,16 @@ import { Request, Response } from 'express';
 
 import { validateRefreshToken } from '@/customValidators/refreshTokenValidator';
 import { customValidationResult } from '@/customValidators/customValidationResults';
+import { generateSecret } from '@/utils/generateSecret';
 
 export const logout = [
   validateRefreshToken,
   async (req: Request, res: Response) => {
     if (!customValidationResult(req).isEmpty() || !req.cookies.refreshToken)
       return res.sendStatus(401);
+
+    const newSecret = generateSecret(10);
+    process.env.SECRET = newSecret;
 
     res.sendStatus(204);
   },
