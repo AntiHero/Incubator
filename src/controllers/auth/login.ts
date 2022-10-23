df2fdb7a2ee73261e6e084373fa917ebc8c8ae75
Aleksandr Schemelev <prophreaker@gmail.com>
2022-10-23 12:39:25 +0300

    test

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -43,11 +43,11 @@ export const login = [
       login,
       password,
     });
-    
+
     console.log(userId, 'userId');
 
     if (!userId) return res.sendStatus(401);
-    
+
     if (!customValidationResult(req).isEmpty()) {
       res
         .type('text/plain')
@@ -67,6 +67,7 @@ export const login = [
 
     const ip = req.ip;
     const userAgent = req.headers['user-agent'];
+    console.log(ip, userAgent)
 
     const newDevice: SecuirityDeviceInput = {
       ip,
@@ -76,14 +77,16 @@ export const login = [
       userId,
     };
 
-    const existingDeviceId = await SecurityService.createDeviceIfNotExists(newDevice);
+    const existingDeviceId = await SecurityService.createDeviceIfNotExists(
+      newDevice
+    );
 
     const userForToken: UserForToken = {
       login,
       userId,
       deviceId: existingDeviceId ?? newDeviceId,
     };
-    
+
     console.log(userForToken.deviceId, 'deviceId');
 
     const [token, refreshToken] = await UsersService.createTokensPair(
@@ -94,5 +97,42 @@ export const login = [
 
     res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true });
     res.status(200).json(payload);
+  },
+];
+[
+  {
+    deviceId: 'b5fde337-edd9-4f45-831e-af7623dccfa3',
+    ip: '3.73.132.52',
+    lastActiveDate: '2022-10-23T09:36:28.919Z',
+    title:
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+  },
+  {
+    deviceId: '236fd8df-3ea5-439d-8b9d-e9a925361c8b',
+    ip: '3.73.132.52',
+    lastActiveDate: '2022-10-23T09:36:21.101Z',
+    title:
+      'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/103.0',
+  },
+  {
+    deviceId: 'bb522135-2c8f-4605-b0b0-36e72564787e',
+    ip: '3.73.132.52',
+    lastActiveDate: '2022-10-23T09:36:21.786Z',
+    title:
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+  },
+  {
+    deviceId: 'afd760d9-dd71-493c-982d-7cf1aed7f517',
+    ip: '3.73.132.52',
+    lastActiveDate: '2022-10-23T09:36:22.466Z',
+    title:
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15',
+  },
+  {
+    deviceId: '9dc4c2b3-5c31-43c9-b77b-4de4beb2255e',
+    ip: '3.73.132.52',
+    lastActiveDate: '2022-10-23T09:36:26.672Z',
+    title:
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
   },
 ];
