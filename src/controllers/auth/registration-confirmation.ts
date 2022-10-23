27c6f6c4cb0155d01e79ade8b144e08a4aaac546
Aleksandr Schemelev <prophreaker@gmail.com>
2022-10-24 01:58:08 +0300

    feat: add rate limit to /auth/registration-confirmation

--- src/controllers/auth/registration-confirmation.ts
+++ src/controllers/auth/registration-confirmation.ts
@@ -1,12 +1,16 @@
 import { body } from 'express-validator';
 import { Request, Response } from 'express';
 
+import * as constants from '@/constants';
 import { ConfirmationRequest } from '@/enums';
-import { APIErrorResult, h05 } from '@/@types';
+import { rateLimit } from '@/utils/rateLimit';
 import * as ErrorMessages from '@/errorMessages';
 import * as UsersService from '@/domain/users.service';
+import { APIErrorResult, h05, IpsType } from '@/@types';
 import { validateConfirmation } from '@/customValidators/confirmationValidator';
 import { customValidationResult } from '@/customValidators/customValidationResults';
+
+const ips: IpsType = {};
 
 export const registrationConfirmation = [
   body(ConfirmationRequest.code)
@@ -16,18 +20,26 @@ export const registrationConfirmation = [
     .custom(validateConfirmation),
   async (req: Request, res: Response) => {
     if (!customValidationResult(req).isEmpty()) {
-      res
-        .type('text/plain')
-        .status(400)
-        .send(
-          JSON.stringify({
-            errorsMessages: customValidationResult(req).array({
-              onlyFirstError: true,
-            }),
-          } as APIErrorResult)
-        );
+      try {
+        const ip = req.ip;
+        
+        rateLimit(ips, ip, constants.RATE_LIMIT, constants.MAX_TIMEOUT);
 
-      return;
+        res
+          .type('text/plain')
+          .status(400)
+          .send(
+            JSON.stringify({
+              errorsMessages: customValidationResult(req).array({
+                onlyFirstError: true,
+              }),
+            } as APIErrorResult)
+          );
+
+        return;
+      } catch (e) {
+        return res.sendStatus(429);
+      }
     }
 
     const user = (await UsersService.findUserByConfirmationCode(
