87578e358c46f0d75061e445e3af6febbed3fd78
Aleksandr Schemelev <prophreaker@gmail.com>
2022-10-23 15:25:49 +0300

    test

--- src/repository/security.repository.ts
+++ src/repository/security.repository.ts
@@ -6,10 +6,10 @@ class SecurityRepository extends Repository<SecurityDevice> {
   async deleteAllButOne (query: { userId: string; deviceId: string }) {
     console.log(await this.collection.find({}).toArray(), 'before delition');
     console.log(query, 'query');
-    await this.collection.deleteMany({
+    console.log(await this.collection.deleteMany({
       userId: query.userId,
       deviceId: { $nin: [query.deviceId] },
-    });
+    }));
   }
 }
 
