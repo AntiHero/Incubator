3a980b6f1802421581716e0cda7406b66f017321
Aleksandr Schemelev <prophreaker@gmail.com>
2022-11-02 21:12:39 +0300

    fix: return comment view

--- src/controllers/comments/getComment.ts
+++ src/controllers/comments/getComment.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 import * as commentsService from '@/app/comments.service';
 import { validateObjectId } from '@/customValidators/objectIdValidator';
+import { convertToCommentViewModel } from '@/utils/convertToCommentView';
 
 export const getComment = [
   validateObjectId,
@@ -12,6 +13,6 @@ export const getComment = [
 
     if (!comment) return res.sendStatus(404);
 
-    res.status(200).type('text/plain').json(comment);
+    res.status(200).type('text/plain').json(convertToCommentViewModel(comment));
   },
 ];
