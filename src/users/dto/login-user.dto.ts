0457d9f68909729ff07c4558c441cd701fb48e39
Aleksandr Schemelev <prophreaker@gmail.com>
2022-11-19 19:45:38 +0300

    fix: omit email

--- src/users/dto/login-user.dto.ts
+++ src/users/dto/login-user.dto.ts
@@ -1,3 +1,5 @@
+import { OmitType } from '@nestjs/mapped-types';
+
 import { CreateUserDto } from './create-user.dto';
 
-export class LoginUserDTO extends CreateUserDto {}
+export class LoginUserDTO extends OmitType(CreateUserDto, ['email']) {}
