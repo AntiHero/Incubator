965dd3432f0330dd3ef4dbba93560d3c0194a838
Aleksandr Schemelev <prophreaker@gmail.com>
2022-11-21 01:11:50 +0300

    fix

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -5,6 +5,7 @@ import { CommentsService } from './comments.service';
 import { CommentsAdapter } from './adapter/mongoose';
 import { CommentModel } from './schemas/comment.schema';
 import { CommentsController } from './comments.controller';
+import { LikeModel } from 'root/likes/schemas/likes.schema';
 
 @Module({
   imports: [
@@ -12,6 +13,12 @@ import { CommentsController } from './comments.controller';
       {
         typegooseClass: CommentModel,
         schemaOptions: { collection: 'comments' },
+      },
+    ]),
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: LikeModel,
+        schemaOptions: { collection: 'likes' },
       },
     ]),
   ],
