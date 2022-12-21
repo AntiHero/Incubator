b19a4c9d5679e1f6243b1661baf40af732035891
Aleksandr Schemelev <prophreaker@gmail.com>
2022-12-21 23:17:24 +0300

    fix: like-status login

--- src/blogs/query/get-blog-post-likes.query.ts
+++ src/blogs/query/get-blog-post-likes.query.ts
@@ -1,4 +1,6 @@
-export const getBlogPostLikesByQuery = `SELECT * FROM "post_likes" 
+export const getBlogPostLikesByQuery = `SELECT post_likes.*, users.login
+    FROM post_likes
+    JOIN users ON users.id=post_likes."userId"
     WHERE "entityId"=$1 AND "likeStatus"='Like'
     ORDER BY "createdAt" DESC
     LIMIT 3
--- src/likes/utils/convertLike.ts
+++ src/likes/utils/convertLike.ts
@@ -2,13 +2,13 @@ import { PostLike } from '../entity/like.entity';
 import { LikeDTO, LikeViewModel } from '../types';
 
 export class ConvertLikeData {
-  static toDTO(like: PostLike): LikeDTO {
+  static toDTO(like: PostLike & { login: string }): LikeDTO {
     return {
       id: String(like.id),
       userId: String(like.userId),
       entityId: String(like.entityId),
       likeStatus: like.likeStatus,
-      login: '',
+      login: like.login,
       isBanned: like.isBanned,
       addedAt: like.createdAt.toISOString(),
     };
