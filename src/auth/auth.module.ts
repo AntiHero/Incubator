bf3b07fefbb4931c505d5540c0c8bcbd980a050e
Aleksandr Schemelev <prophreaker@gmail.com>
2023-01-09 13:46:30 +0300

    test

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,11 +13,9 @@ import { User } from 'root/users/entity/user.entity';
 import { UsersModule } from 'root/users/users.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { TokensModule } from 'root/tokens/tokens.module';
-// import { ConfirmUserUseCase } from 'root/users/use-cases/confirm-user.use-case';
 import { SecurityDevicesModule } from 'root/security-devices/security-devices.module';
 import { ValidateRecoveryCode } from 'root/@common/decorators/validate-uuid.decorator';
-// import { IpRestrictionMiddleware } from 'root/@common/middlewares/ip-restriction.middleware';
-// import { ConfirmationInfoSqlRepository } from 'root/users/adapter/user-confirmation-info-sql.adapter';
+import { IpRestrictionMiddleware } from 'root/@common/middlewares/ip-restriction.middleware';
 import { PasswordAuthorizationMiddleware } from 'root/@common/middlewares/password-authorization.middleware';
 import { RefreshTokenValidationMiddleware } from 'root/@common/middlewares/refresh-token.validation.middleware';
 
@@ -35,22 +33,22 @@ import { RefreshTokenValidationMiddleware } from 'root/@common/middlewares/refre
 export class AuthModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      // .apply(IpRestrictionMiddleware)
-      // .forRoutes(
-      //   {
-      //     path: 'auth/registration',
-      //     method: RequestMethod.POST,
-      //   },
-      //   {
-      //     path: 'auth/registration-confirmation',
-      //     method: RequestMethod.POST,
-      //   },
-      //   {
-      //     path: 'auth/registration-email-resending',
-      //     method: RequestMethod.POST,
-      //   },
-      //   { path: 'auth/login', method: RequestMethod.POST },
-      // )
+      .apply(IpRestrictionMiddleware)
+      .forRoutes(
+        {
+          path: 'auth/registration',
+          method: RequestMethod.POST,
+        },
+        {
+          path: 'auth/registration-confirmation',
+          method: RequestMethod.POST,
+        },
+        {
+          path: 'auth/registration-email-resending',
+          method: RequestMethod.POST,
+        },
+        { path: 'auth/login', method: RequestMethod.POST },
+      )
       .apply(PasswordAuthorizationMiddleware)
       .forRoutes({
         path: 'auth/login',
