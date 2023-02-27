3d17b9973e3c3287e75da7ed1ee653e16b16e173
Aleksandr Schemelev <prophreaker@gmail.com>
2023-02-27 21:18:49 +0300

    fix: change signed url to public

--- src/bloggers/application/services/cloud.service.ts
+++ src/bloggers/application/services/cloud.service.ts
@@ -55,12 +55,13 @@ export class CloudService {
 
     const uploadedImage = this.bucket.file(fileName);
 
-    const url = (
-      await uploadedImage.getSignedUrl({
-        action: 'read',
-        expires: '01-01-2100',
-      })
-    )[0];
+    const url = uploadedImage.publicUrl();
+    // (
+    //   await uploadedImage.getSignedUrl({
+    //     action: 'read',
+    //     expires: '01-01-2100',
+    //   })
+    // )[0];
 
     const metadata = await sharp(file.buffer).metadata();
     const { size, width, height } = metadata;
